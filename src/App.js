a9e22dff23930098d4febdcdd0d5ea00bcfd3e9c
abishek <abishekbalaji7.151@gmail.com>
2020-10-27 19:48:56 +0530

    Navbar set up

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import "./App.css";
 
 import Homepage from "./pages/homepage/Homepage.component";
 import ShopPage from "./pages/shopPage/ShopPage.component";
+import Header from "./components/header/Header.component";
 
 const App = () => (
   <div>
+    <Header />
     <Switch>
       <Route path="/" component={Homepage} exact />
       <Route path="/shop" component={ShopPage} />
