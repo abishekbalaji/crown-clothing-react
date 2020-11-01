dd92be1cf8decfe6138c0fb8bb6db73410e18f2a
Abishek B <abishekbalaji7.151@gmail.com>
2020-11-01 16:25:27 +0530

    Set up sign up using firestore

--- src/pages/signInandSignUp/signInandSignUp.component.jsx
+++ src/pages/signInandSignUp/signInandSignUp.component.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./signInandSignUp.styles.scss";
 
 import SignIn from "../../components/signin/SignIn.component";
+import SignUp from "../../components/signUp/SignUp.component";
 
 const SignInandSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
