87235c1beff0b1890f787586526528d35d779871
Abishek B <abishekbalaji7.151@gmail.com>
2020-11-01 17:19:24 +0530

    Sign in with firebase complete

--- src/components/signin/SignIn.component.jsx
+++ src/components/signin/SignIn.component.jsx
@@ -54,7 +54,7 @@ export default class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
               Sign In with Google
             </CustomButton>
           </div>
