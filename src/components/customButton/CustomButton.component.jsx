0ee7d891be234a46659143cabfa1a8bf88f95d44
Abishek B <abishekbalaji7.151@gmail.com>
2020-11-02 22:23:53 +0530

    Updated shop data, set up utility function to group items in cart

--- src/components/customButton/CustomButton.component.jsx
+++ src/components/customButton/CustomButton.component.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 import "./CustomButton.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`custom-button ${isGoogleSignIn ? "google-sign-in" : ""}`}
+    className={`custom-button ${isGoogleSignIn ? "google-sign-in" : ""} ${
+      inverted ? "inverted" : ""
+    }`}
     {...otherProps}
   >
     {children}
